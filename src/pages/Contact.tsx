7b73642458a762f06560e25f989692ce3d324abd
hbmarkofficial-business <hbmark.official@gmail.com>
2026-01-27 22:21:21 +0100

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -131,14 +131,14 @@ const Contact = () => {
 
                   <div className="aspect-video rounded-sm overflow-hidden border border-border">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2593.0466369434594!2d11.117062376893147!3d49.464733457845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f57dec87eba7f%3A0xac674ba124944238!2sLord%20Barbershop%20Coiffeur!5e0!3m2!1sde!2sde!4v1769174581720!5m2!1sde!2sde"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5188.1897957256715!2d11.063459092714709!3d49.44492338556231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f5707238b72c3%3A0xf3c800468a66d8fd!2sBarbershop%20SARKAUT!5e0!3m2!1sde!2sde!4v1769549168103!5m2!1sde!2sde"
                       width="100%"
                       height="100%"
                       style={{ border: 0, filter: 'grayscale(100%) contrast(1.2)' }}
                       allowFullScreen
                       loading="lazy"
                       referrerPolicy="no-referrer-when-downgrade"
-                      title="Standort Lord Barbershop Coiffeur"
+                      title="Standort Barbershop SARKAUT"
                     />
                   </div>
                 </div>
@@ -154,6 +154,7 @@ const Contact = () => {
 };
 
 export default Contact;
+
 
 
 
